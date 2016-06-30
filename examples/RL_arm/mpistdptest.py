3dcc98067e6490e953fdc256eb94b984e0f0cb2b
cliffckerr <questionably@gmail.com>
2016-06-30 13:06:42 +0800

    can run sim for 2 s at 7 hz firing in 1.8 s

--- examples/RL_arm/mpistdptest.py
+++ examples/RL_arm/mpistdptest.py
@@ -6,10 +6,12 @@
 """
 
 from neuron import h, init, run
-from pylab import figure, plot, show, array
+from pylab import figure, plot, show, array, rand, arange
+from optima import tic, toc
 
-duration = 400
-which = 'izhi' # Choices are: izhi, izhi2007b, intfire4
+duration = 2000
+ncells = 200
+connprob = 0.1
 
 ## Create basic Izhikevich neuron with default parameters. Not to be called directly, only via one of the other functions.
 def createcell(sec, C, k, vr, vt, vpeak, a, b, c, d, celltype, cellid):
@@ -33,13 +35,14 @@ def pyramidal(sec, C=100, k=3, vr=-60, vt=-50, vpeak=30, a=0.01, b=5, c=-60, d=4
     return cell
 
 ## Create cells
-ncells = 2
 cells = []
-seclist = []
-for c in range(ncells):
+secs = []
+allcells = arange(ncells)
+for c in allcells:
     sec = h.Section() # Create a dummy section to put the point processes in
-    seclist.append(sec)
-    cells.append(pyramidal(section=sec)) # Create the cells
+    cell = pyramidal(sec) # Create the cells
+    secs.append(sec)
+    cells.append(cell)
     
 
 
@@ -47,23 +50,41 @@ for c in range(ncells):
 ## Create synapses
 threshold = 40 # Set voltage threshold
 delay = 1 # Set connection delay
-singlesyn = h.NetCon(cells[0],cells[1], threshold, delay, 0.5) # Create a connection between the cells
-singlesyn.weight[0]=10
-stdpmech = h.STDP(0,sec=sec) # Create the STDP mechanism
-stdpmech.verbose = 0.0
-delay = 1 # Set connection delay
-presyn = h.NetCon(cells[0],stdpmech, threshold, delay, 1) # Feed presynaptic spikes to the STDP mechanism -- must have weight >0
-pstsyn = h.NetCon(cells[1],stdpmech, threshold, delay, -1) # Feed postsynaptic spikes to the STDP mechanism -- must have weight <0
-h.setpointer(singlesyn._ref_weight[0],'synweight',stdpmech) # Point the STDP mechanism to the connection weight
+singlesyns = []
+stdpmechs = []
+presyns = []
+pstsyns = []
+precells = allcells[rand(ncells)<connprob]
+pstcells = allcells[rand(ncells)<connprob]
+for c1 in precells:
+    for c2 in pstcells:
+        if c1!=c2:
+            singlesyn = h.NetCon(cells[c1],cells[c2], threshold, delay, 0.5) # Create a connection between the cells
+            singlesyn.weight[0]=10
+            stdpmech = h.STDP(0,sec=secs[c1]) # Create the STDP mechanism
+            stdpmech.verbose = 0.0
+            delay = 1 # Set connection delay
+            presyn = h.NetCon(cells[c1],stdpmech, threshold, delay, 1) # Feed presynaptic spikes to the STDP mechanism -- must have weight >0
+            pstsyn = h.NetCon(cells[c2],stdpmech, threshold, delay, -1) # Feed postsynaptic spikes to the STDP mechanism -- must have weight <0
+            h.setpointer(singlesyn._ref_weight[0],'synweight',stdpmech) # Point the STDP mechanism to the connection weight
+            singlesyns.append(singlesyn)
+            stdpmechs.append(stdpmech)
+            presyns.append(presyn)
+            pstsyns.append(pstsyn)
 
-source = h.NetStim()                    # Create a NetStim
-source.interval = 1#(float(50)/1e3)**-1   # Interval between spikes
-source.number = duration                      # Number of spikes
-source.noise = 1                        # Fractional noise in timing
-source.start = 25                       # Delay 
-        
-connection = h.NetCon(source, cells[0])
-connection.weight[0] = 1
+sources = []
+sourceconns = []
+for c in allcells:
+    source = h.NetStim()                    # Create a NetStim
+    source.interval = 1#(float(50)/1e3)**-1   # Interval between spikes
+    source.number = duration                      # Number of spikes
+    source.noise = 1                        # Fractional noise in timing
+    source.start = 25                       # Delay 
+    
+    sourceconn = h.NetCon(source, cells[c])
+    sourceconn.weight[0] = 1
+    sources.append(source)
+    sourceconns.append(sourceconn)
 
 
 
@@ -75,26 +96,22 @@ wvec.record(singlesyn._ref_weight[0])
 
 evec1 = h.Vector()
 evec2 = h.Vector()
-if which=='izhi2007b':
-    print("WARNING, doesn't work")
-    ptr1 = seclist[0](0.5)._ref_v
-    ptr2 = seclist[1](0.5)._ref_v
-if which=='izhi':
-    ptr1 = cells[0]._ref_V
-    ptr2 = cells[1]._ref_V
-if which=='intfire4':
-    ptr1 = cells[0]._ref_m
-    ptr2 = cells[1]._ref_m
+ptr1 = cells[0]._ref_V
+ptr2 = cells[1]._ref_V
 evec1.record(ptr1)
 evec2.record(ptr2)
 
+
+
 init()
+t = tic()
 run(duration)
+toc(t)
 
 figure(1)
 plot(array(tvec),array(wvec),'r')
 #ylim(-0.1,1.1)
 figure(2)
-plot(array(tvec),array(evec1))
+#plot(array(tvec),array(evec1))
 plot(array(tvec),array(evec2))
 show()
